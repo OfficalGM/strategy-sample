3c519f96436140c378541c49013053f7711cc624
Tony <tony.wu@vpon.com>
2020-12-14 17:30:18 +0800

    refactoring when shipperName is not match problem

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,16 +1,18 @@
+import java.util.HashMap;
+
 public class Cart {
 
+    private HashMap<String, Shipper> shipperMap = new HashMap<>() {{
+        put("black cat", new BlackCat());
+        put("hsinchu", new Hsinchu());
+        put("post office", new PostOffice());
+    }};
+
     public double shippingFee(String shipperName, Product product) {
-        switch (shipperName) {
-            case "black cat":
-                return new BlackCat().calculateFee(product);
-            case "hsinchu":
-                return new Hsinchu().calculateFee(product);
-            case "post office":
-                return new PostOffice().calculateFee(product);
-            default:
-                throw new IllegalArgumentException("shipperName not exist");
+        if (!shipperMap.containsKey(shipperName)) {
+            throw new IllegalArgumentException("shipper not exist");
         }
+        return shipperMap.get(shipperName).calculateFee(product);
     }
 
 }
